7400d4d080852ec848cddf3fd459dfb4ed1542d3
Eliot Valdes <eliottvaldes@hotmail.com>
2024-10-04 23:40:39 -0600

    ref: refactor code to make it more clear removing unnecessary comments

--- calculate_aptitude.py
+++ calculate_aptitude.py
@@ -6,41 +6,31 @@ import cv2
 @context: General approach to evaluate the individuals in the population
 @param population: np.ndarray -> All population to be evaluated -> F.E: [[2.3, 4.5], [1.4, -0.2]]
 @param objective_function: function -> Objective function to evaluate the individuals
+@param of_params: Any additional parameter needed by the objective function. could change depending on the objective function
 @return: np.ndarray -> Aptitude of the individuals in the population (Vector) [9, 5]
 """
 def evaluate_population(population: np.ndarray, objective_function, of_params) -> np.ndarray:
-    # get the number of individuals and variables
     n_individuals, n_var = population.shape
-    # create the aptitude vector
     aptitude = np.zeros(n_individuals)
-    # loop over the population
     for i in range(n_individuals):
-        # get the individual
         individual = population[i]
-        # evaluate the individual
         aptitude[i] = objective_function(individual, of_params)
     return aptitude
 
 
 def apply_sigmoid(img, alpha: float, delta: float)-> float:
     output_img = 1 / (1 + np.exp(alpha * (delta - img)))
-    # Reescalar la imagen al rango [0, 1]
     output_img = cv2.normalize(output_img, None, 0, 1, cv2.NORM_MINMAX)
     return output_img
 
 
 def calculate_shannon_entropy(img) -> float:
     img_flat = img.flatten()
-    # Calcular el histograma (counts)
     hist_counts, _ = np.histogram(img_flat, bins=256, range=(0, 1), density=False)
-    # Normalizar para obtener probabilidades
     total_pixels = np.sum(hist_counts)
     probabilities = hist_counts / total_pixels
-    # Eliminar ceros para evitar log(0)
     probabilities = probabilities[probabilities > 0]
-    # Calcular la entropía de Shannon
-    shannon_entropy = -np.sum(probabilities * np.log2(probabilities))
-    return shannon_entropy    
+    return -np.sum(probabilities * np.log2(probabilities))    
 
 
 def obj_func_shannon_entropy(individual: np.ndarray, image: np.ndarray) -> float:
@@ -50,28 +40,20 @@ def obj_func_shannon_entropy(individual: np.ndarray, image: np.ndarray) -> float
 
 
 def calculate_spatial_entropy(img: np.ndarray) -> float:
-    grad_mags = np.zeros(img.shape[:2])
+    grad_mags = np.zeros(img.shape[:2])    
     for i in range(3):
         channel = img[:, :, i]
-        # calculate the gradients in x and y directions
         grad_x = cv2.Sobel(channel, cv2.CV_64F, 1, 0, ksize=3)
         grad_y = cv2.Sobel(channel, cv2.CV_64F, 0, 1, ksize=3)
-        # calculate the gradient magnitude
         grad_mag = np.sqrt(grad_x**2 + grad_y**2)
-        grad_mags += grad_mag  # sum the gradient magnitudes
-
-    # normalize the gradient magnitudes to the range [0, 1]
+        grad_mags += grad_mag
+        
     grad_mags = grad_mags / np.max(grad_mags)
-    # calculate the histogram of gradient magnitudes
     hist_counts, _ = np.histogram(grad_mags.flatten(), bins=256, range=(0, 1), density=False)
-    # calculate the probabilities
     total_pixels = np.sum(hist_counts)
     probabilities = hist_counts / total_pixels
-    # remove zeros to avoid log(0)
     probabilities = probabilities[probabilities > 0]
-    # calculate the spatial entropy
-    spatial_entropy = -np.sum(probabilities * np.log2(probabilities))
-    return spatial_entropy
+    return -np.sum(probabilities * np.log2(probabilities))
 
 
 def obj_func_shannon_spatial_entropy(individual: np.ndarray, image: np.ndarray) -> float:
@@ -83,15 +65,16 @@ def obj_func_shannon_spatial_entropy(individual: np.ndarray, image: np.ndarray) 
 
 if __name__ == '__main__':
     # JUST FOR TESTING
-    # GENERAL CONFIGURATIONS
+    # simulate a population
     population = np.array([[2.3 , 4.5], [1.4, -0.2]])
-    
+    # simulate an image
+    image = np.random.rand(400, 400)    
     # evaluate the population
-    aptitude = evaluate_population(population, image_objective_function, [np.random.rand(400, 400)])
-    # get the index of the minimum aptitude
-    best_index = np.argmin(aptitude)
+    aptitude = evaluate_population(population, obj_func_shannon_entropy, image)
+    # get the index of the max aptitude
+    best_index = np.argmax(aptitude)    
     
-    print(f'Population: \n{population}\n')
-    print(f'Aptitude: {aptitude}')
+    print(f'Population: \n{population}')
+    print(f'Aptitude Vector: {aptitude}')
     print(f'Best index: {best_index}')
     print(f'Best individual: {population[best_index]}')   
