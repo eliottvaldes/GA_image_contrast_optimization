5b27707c7d437f86b1fd756bea2db5c715d1caad
Eliot Valdes <eliottvaldes@hotmail.com>
2024-10-08 00:24:07 -0600

    feat: add new configuration keys

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from ga_complete import run_ga
 
 # => Image configurations
-img_path = 'assets/microphotographs-of-pulmonary-blood-vessels.png'
+img_path = 'assets/White-blood-cell-(basophil).jpg'
 img_max_height = 900
 
 # GA configuration
 ga_config = {
-    'population_size': 10,
-    'generations': 1,
+    'population_size': 50,
+    'generations': 10,
     'variables': 2,
     'limits': list([[0, 1], [0, 10]]),
     'sbx_prob': 0.8,
     'sbx_dispersion_param': 3,
     'mutation_probability_param': 0.7,
     'distribution_index_param': 95,
-    'objetive_function': 'spatial_entropy', # 'spatial_entropy' or 'shannon_entropy'
+    # extra configuration
+    'pdi_function_4_improvement': 'sigmoid', # 'sigmoid' or 'clahe'
+    'objetive_function': 'shannon_entropy', # 'spatial_entropy' or 'shannon_entropy'
     'parent_selection_optimization': 'maximize', # 'maximize' or 'minimize'
     'image_path': img_path,
     'img_max_height': img_max_height,
+    'dynamic_sbx_increasing': False,
+    'early_stop': False,
 }
 
 save_log = True
-show_image_result = False
+show_image_result = True
 
 # =======================================
 # RUN THE GA
