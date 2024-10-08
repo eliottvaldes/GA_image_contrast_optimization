9be0fc7a01cb0505321ec6ce78b8ad687039f7a5
Eliot Valdes <eliottvaldes@hotmail.com>
2024-10-08 02:51:25 -0600

    ref: improve limits GA configuration depends on the dip function selected

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from ga_complete import run_ga
 
 # => Image configurations
-img_path = 'assets/4360.png'
+img_path = 'assets/bones/4370.png'
 img_max_height = 900
 
 # define the Digital Image Processing function to be used to transform the image
 dip_function = 'clahe' # 'sigmoid' or 'clahe'
+
+# define the limits dynamically
+limits = []
+if dip_function == 'sigmoid':
+    # limits 4 sigmoid function
+    limits = [[0, 1], [0, 10]]
+else:
+    # limits 4 clahe function
+    limits = [[1, 4], [1, 30]]
+    if 'bones' in img_path:
+        # new limits for bones images 2 get better results
+        limits = [[4, 11], [1, 50]]
+        
 
 # GA configuration
 ga_config = {
     'population_size': 150,
     'generations': 10,
     'variables': 2,
-    'limits': [[0, 1], [0, 10]] if dip_function == 'sigmoid' else [[1, 4], [1, 30]],
+    'limits': limits,
     'sbx_prob': 0.8,
     'sbx_dispersion_param': 3,
     'mutation_probability_param': 0.7,
