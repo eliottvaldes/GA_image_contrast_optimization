aca6fa86e3a5fdcb35f6ddce4e49fb53c7fdabd5
Eliot Valdes <eliottvaldes@hotmail.com>
2024-10-09 02:32:36 -0600

    test: implement single function test

--- ga_complete.py
+++ ga_complete.py
@@ -130,3 +130,61 @@ def run_ga(ga_config: dict, save_log: bool, show_image_result: bool = False):
     if show_image_result:
         # show the image
         plot_results(image, ga_result, ga_config['objetive_function'].__name__, ga_config['dip_function_4_improvement'])
+        
+        
+        
+if __name__ == '__main__':
+    
+    import json
+           
+    # => Image configurations
+    img_path = 'assets/White-blood-cell-(basophil).jpg'
+    img_max_height = 900
+
+    # define the Digital Image Processing function to be used to transform the image
+    dip_function = 'sigmoid' # 'sigmoid' or 'clahe'
+    
+    # GA configuration  
+    ga_config = {
+        'population_size': 60,
+        'generations': 200,
+        'variables': 2,
+        'limits': [],
+        'sbx_prob': 0.8,
+        'sbx_dispersion_param': 3,                              
+        'mutation_probability_param': 0.7,
+        'distribution_index_param': 95,
+        # extra configuration
+        'dip_function_4_improvement': dip_function,
+        'objetive_function': 'shannon_entropy', # 'spatial_entropy' or 'shannon_entropy'
+        'parent_selection_optimization': 'maximize', # 'maximize' or 'minimize'
+        'image_path': img_path,
+        'img_max_height': img_max_height,
+        'dynamic_sbx_increasing': True,
+        'early_stop': False,
+    }
+
+    save_log = True
+    show_image_result = True
+
+    # define the limits dynamically
+    if dip_function == 'sigmoid':
+        # limits 4 sigmoid function
+        limits = [[0, 1], [0, 10]]
+    else:
+        # limits 4 clahe function
+        limits = [[1, 4], [1, 30]]
+        if 'bones' in img_path:
+            # new limits for bones images 2 get better results
+            limits = [[4, 11], [1, 50]]
+    ga_config['limits'] = limits
+
+    # ---------------------------------------
+    # Show the configuration
+    # ---------------------------------------
+    print(f"=> GA Configuration: \n{json.dumps(ga_config, indent=4)}")
+
+    # =======================================
+    # RUN THE GA
+    # =======================================
+    run_ga(ga_config, save_log, show_image_result)
